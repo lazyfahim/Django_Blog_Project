0e1210781d697f6fef11cfce2898618448446a40
mahdi-jewel <mahdi15-1078@diu.edu.bd>
2020-06-17 17:39:38 +0600

    project upload

---
+++ My_Blog/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\mahdi jewel\desktop\projects_django\my_blog\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
